1689adb7a0277454fa58b90db44aa1a15b11c249
drewcsontos <drew@csontos.net>
2020-06-11 17:04:18 -0400

    Restructured all

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 document.addEventListener("keydown", function (event) {
     if (event.code == "KeyA" || event.code == "KeyS" || event.code == "KeyD" || event.code == "KeyF" || event.code == "KeyG" || event.code == "KeyH" || event.code == "KeyJ" || event.code == "KeyW" || event.code == "KeyE" || event.code == "KeyT" || event.code == "KeyY" || event.code == "KeyU") {
-        let audio = new Audio("audio/" + event.key.toUpperCase() + ".mp3");
-        audio.play();
+        playNote(event.key);
     } else
         console.log("Incorrect key")
+
 })
+
+function playNote(key) {
+    let audio = new Audio("audio/" + key.toUpperCase() + ".mp3");
+    audio.play();
+}
