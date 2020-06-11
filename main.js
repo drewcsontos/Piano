a4b11db138e15ca1c9ca4a78b5ac9d7f1865136d
drewcsontos <drew@csontos.net>
2020-06-11 19:18:43 -0400

    Added event listeners

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 document.addEventListener("keydown", function (event) {
     if (event.code == "KeyA" || event.code == "KeyS" || event.code == "KeyD" || event.code == "KeyF" || event.code == "KeyG" || event.code == "KeyH" || event.code == "KeyJ" || event.code == "KeyW" || event.code == "KeyE" || event.code == "KeyT" || event.code == "KeyY" || event.code == "KeyU") {
+
         playNote(event.key);
     } else
         console.log("Incorrect key")
 
 })
+document.querySelectorAll("kbd").forEach(x => {
+    x.addEventListener("click", function(){playNote(x.textContent)},false)
+})
 
 function playNote(key) {
+    console.log(key);
     let audio = new Audio("audio/" + key.toUpperCase() + ".mp3");
     audio.play();
 }
-document.addEventListener("load", function (event) {
-    ["A", "D", "E", "F", "G", "H", "S", "T", "U", "W", "Y"].map(key => {
-        let x = new Audio("audio/" + key + ".mp3");
-    })
-})
